686dec019acf3b0534765c0e8882cbfc0af405e0
Chris Bongers <chrisbongers@gmail.com>
2021-07-06 08:53:32 +0200

    Changed to bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,50 @@ import 'package:flutter/material.dart';
 
 void main() async {
   runApp(
-    MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Tabs Demo'),
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit)),
-                Tab(icon: Icon(Icons.directions_bike)),
-              ],
-            ),
-          ),
-          body: TabBarView(
-            children: [
-              Icon(Icons.directions_car),
-              Icon(Icons.directions_transit),
-              Icon(Icons.directions_bike),
-            ],
-          ),
-        ),
-      ),
-    ),
+    MaterialApp(home: BottomTabBarWidget()),
   );
+}
+
+class BottomTabBarWidget extends StatefulWidget {
+  @override
+  State<BottomTabBarWidget> createState() => _BottomTabBarWidget();
+}
+
+class _BottomTabBarWidget extends State<BottomTabBarWidget> {
+  int _selectedIndex = 0;
+  static const List _tabPages = [
+    Text('I travel by Car'),
+    Text('I like trains more'),
+    Text('I like to ride my bycycle'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tabs Demo'),
+      ),
+      body: Center(
+        child: _tabPages[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.directions_car), label: 'Car'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.directions_transit), label: 'Train'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.directions_bike), label: 'Bike'),
+        ],
+      ),
+    );
+  }
 }

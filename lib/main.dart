98d8906f5872f02e60f2cf9cd7f3605b10e37c19
Chris Bongers <chrisbongers@gmail.com>
2021-07-08 21:20:14 +0200

    Flutter import existing dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/home.dart';
 
 void main() async {
   runApp(
     MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()),
   );
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-              ),
-              TextButton(
-                onPressed: _incrementCounter,
-                child: const Text('Add number'),
-              ),
-            ]),
-      ),
-    );
-  }
 }
---
+++ lib/screens/home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+              ),
+              TextButton(
+                onPressed: _incrementCounter,
+                child: const Text('Add number'),
+              ),
+            ]),
+      ),
+    );
+  }
+}

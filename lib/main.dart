549a50d1cdf2791e58ad5ff6e6b27b4d7878deff
Chris Bongers <chrisbongers@gmail.com>
2021-07-13 07:58:58 +0200

    Passing data to a route

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ void main() async {
   runApp(
     MaterialApp(debugShowCheckedModeBanner: false, initialRoute: '/', routes: {
       '/': (context) => RouteOne(),
-      '/detail': (context) => RouteTwo(),
+      '/detail': (context) => RouteTwo(item: ''),
     }),
   );
 }
 
 class RouteOne extends StatelessWidget {
+  final list = List.generate(20, (index) => 'Item $index');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,13 +19,23 @@ class RouteOne extends StatelessWidget {
         title: Text('Screen one ☝️'),
       ),
       body: Center(
-        child: ElevatedButton(
-          // Within the `FirstScreen` widget
-          onPressed: () {
-            // Navigate to the second screen using a named route.
-            Navigator.pushNamed(context, '/detail');
+        child: ListView.builder(
+          itemCount: list.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(list[index]),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RouteTwo(
+                      item: list[index],
+                    ),
+                  ),
+                );
+              },
+            );
           },
-          child: Text('Open detail'),
         ),
       ),
     );
@@ -31,6 +43,10 @@ class RouteOne extends StatelessWidget {
 }
 
 class RouteTwo extends StatelessWidget {
+  final String item;
+
+  RouteTwo({Key? key, required this.item}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,15 +54,25 @@ class RouteTwo extends StatelessWidget {
         title: Text('Screen two ✌️'),
       ),
       body: Center(
-        child: ElevatedButton(
-          // Within the `FirstScreen` widget
-          onPressed: () {
-            // Navigate to the second screen using a named route.
-            Navigator.pop(context);
-          },
-          child: Text('Go back'),
-        ),
-      ),
+          child: Column(
+        children: [
+          Spacer(),
+          Text(
+            'You clicked on: $item',
+            style: TextStyle(fontSize: 32),
+          ),
+          Spacer(),
+          ElevatedButton(
+            // Within the `FirstScreen` widget
+            onPressed: () {
+              // Navigate to the second screen using a named route.
+              Navigator.pop(context);
+            },
+            child: Text('Go back'),
+          ),
+          Spacer(),
+        ],
+      )),
     );
   }
 }

b4cbc0e05e997063c6fb91894fab4139c3bdd58d
Chris Bongers <chrisbongers@gmail.com>
2021-07-24 10:54:11 +0200

    Created a grid view demo

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,65 @@ void main() async {
   runApp(
     MaterialApp(debugShowCheckedModeBanner: false, initialRoute: '/', routes: {
       '/': (context) => RouteOne(),
-      '/detail': (context) => RouteTwo(item: ''),
+      '/detail': (context) => RouteTwo(image: '', name: ''),
     }),
   );
 }
 
+class PhotoItem {
+  final String image;
+  final String name;
+  PhotoItem(this.image, this.name);
+}
+
 class RouteOne extends StatelessWidget {
-  final list = List.generate(20, (index) => 'Item $index');
+  final List<PhotoItem> _items = [
+    PhotoItem(
+        "https://images.pexels.com/photos/1772973/pexels-photo-1772973.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "Stephan Seeber"),
+    PhotoItem(
+        "https://images.pexels.com/photos/1758531/pexels-photo-1758531.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "Liam Gant"),
+    PhotoItem(
+        "https://images.pexels.com/photos/1130847/pexels-photo-1130847.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "Stephan Seeber"),
+    PhotoItem(
+        "https://images.pexels.com/photos/45900/landscape-scotland-isle-of-skye-old-man-of-storr-45900.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "Pixabay"),
+    PhotoItem(
+        "https://images.pexels.com/photos/165779/pexels-photo-165779.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "Scott Webb"),
+    PhotoItem(
+        "https://images.pexels.com/photos/548264/pexels-photo-548264.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "Krivec Ales"),
+    PhotoItem(
+        "https://images.pexels.com/photos/188973/matterhorn-alpine-zermatt-mountains-188973.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "Pixabay"),
+    PhotoItem(
+        "https://images.pexels.com/photos/795188/pexels-photo-795188.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "Melanie Wupper"),
+    PhotoItem(
+        "https://images.pexels.com/photos/5222/snow-mountains-forest-winter.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "Jaymantri"),
+    PhotoItem(
+        "https://images.pexels.com/photos/789381/pexels-photo-789381.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "Riciardus"),
+    PhotoItem(
+        "https://images.pexels.com/photos/326119/pexels-photo-326119.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "Pixabay"),
+    PhotoItem(
+        "https://images.pexels.com/photos/707344/pexels-photo-707344.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "Eberhard"),
+    PhotoItem(
+        "https://images.pexels.com/photos/691034/pexels-photo-691034.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "Mirsad Mujanovic"),
+    PhotoItem(
+        "https://images.pexels.com/photos/655676/pexels-photo-655676.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "Vittorio Staffolani"),
+    PhotoItem(
+        "https://images.pexels.com/photos/592941/pexels-photo-592941.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "Tobi"),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -18,34 +70,45 @@ class RouteOne extends StatelessWidget {
       appBar: AppBar(
         title: Text('Screen one ☝️'),
       ),
-      body: Center(
-        child: ListView.builder(
-          itemCount: list.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(list[index]),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => RouteTwo(
-                      item: list[index],
-                    ),
-                  ),
-                );
-              },
-            );
-          },
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisSpacing: 0,
+          mainAxisSpacing: 0,
+          crossAxisCount: 3,
         ),
+        itemCount: _items.length,
+        itemBuilder: (context, index) {
+          return new GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => RouteTwo(
+                      image: _items[index].image, name: _items[index].name),
+                ),
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(_items[index].image),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
 }
 
 class RouteTwo extends StatelessWidget {
-  final String item;
+  final String image;
+  final String name;
 
-  RouteTwo({Key? key, required this.item}) : super(key: key);
+  RouteTwo({Key? key, required this.image, required this.name})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -53,26 +116,28 @@ class RouteTwo extends StatelessWidget {
       appBar: AppBar(
         title: Text('Screen two ✌️'),
       ),
-      body: Center(
-          child: Column(
+      body: Column(
         children: [
-          Spacer(),
-          Text(
-            'You clicked on: $item',
-            style: TextStyle(fontSize: 32),
+          AspectRatio(
+            aspectRatio: 1,
+            child: Container(
+              width: double.infinity,
+              child: Image(
+                image: NetworkImage(image),
+              ),
+            ),
           ),
-          Spacer(),
-          ElevatedButton(
-            // Within the `FirstScreen` widget
-            onPressed: () {
-              // Navigate to the second screen using a named route.
-              Navigator.pop(context);
-            },
-            child: Text('Go back'),
+          Container(
+            margin: const EdgeInsets.all(20.0),
+            child: Center(
+              child: Text(
+                name,
+                style: TextStyle(fontSize: 40),
+              ),
+            ),
           ),
-          Spacer(),
         ],
-      )),
+      ),
     );
   }
 }

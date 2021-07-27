8d877bf1c13d7dd7c709ac1d9303f01026910d1a
Chris Bongers <chrisbongers@gmail.com>
2021-07-27 07:36:59 +0200

    Added horizontal scroll list

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ void main() async {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('Scroll'),
+        ),
         body: MyApp(),
       ),
     ),
@@ -14,10 +17,41 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Hello World 👋',
-        textDirection: TextDirection.ltr,
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      height: 140.0,
+      child: ListView.separated(
+        itemCount: 10,
+        separatorBuilder: (BuildContext context, int index) {
+          return SizedBox(
+            width: 10,
+          );
+        },
+        itemBuilder: (_, i) => SampleAvatar(),
+        scrollDirection: Axis.horizontal,
+      ),
+    );
+  }
+}
+
+class SampleAvatar extends StatelessWidget {
+  const SampleAvatar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 120.0,
+      height: 120.0,
+      padding: EdgeInsets.all(2),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.black,
+      ),
+      child: CircleAvatar(
+        backgroundImage: NetworkImage(
+            'https://media-exp1.licdn.com/dms/image/C4D03AQFgZBilNtPUMA/profile-displayphoto-shrink_800_800/0/1604728137407?e=1632960000&v=beta&t=QKa1Nq3WKWQGEGaiKdZ1ovp1h6uAbwPZfihdqY2_pNU'),
       ),
     );
   }

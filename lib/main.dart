07ae02a78e8388197926e42dbbc89746bb056ad5
Chris Bongers <chrisbongers@gmail.com>
2021-07-28 08:20:56 +0200

    Add gradient text example

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,45 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  final Shader linearGradient = LinearGradient(
+    colors: <Color>[Colors.pink, Colors.green],
+  ).createShader(
+    Rect.fromLTWH(0.0, 0.0, 200.0, 70.0),
+  );
+
+  final gradient = LinearGradient(
+    colors: [Colors.pink, Colors.green],
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Hello World 👋',
-        textDirection: TextDirection.ltr,
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Center(
+          child: Text(
+            'Hello Gradients!',
+            style: new TextStyle(
+                fontSize: 60.0,
+                fontWeight: FontWeight.bold,
+                foreground: Paint()..shader = linearGradient),
+          ),
+        ),
+        ShaderMask(
+          shaderCallback: (Rect bounds) {
+            return gradient.createShader(Offset.zero & bounds.size);
+          },
+          child: Text(
+            'Hello Gradients!',
+            style: new TextStyle(
+              color: Colors.white,
+              fontSize: 60.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }

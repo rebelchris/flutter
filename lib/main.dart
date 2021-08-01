b0fe97a806e4a38dfeecf904ae1218d3faf9c69a
Chris Bongers <chrisbongers@gmail.com>
2021-08-01 13:36:56 +0200

    Added drawer menu

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,7 @@ void main() async {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: MyApp(),
-      ),
+      home: MyApp(),
     ),
   );
 }
@@ -14,10 +12,39 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Hello World 👋',
-        textDirection: TextDirection.ltr,
+    return Scaffold(
+      appBar: AppBar(title: Text('Drawer menu')),
+      body: const Center(
+        child: Text('Click on the menu icon'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }

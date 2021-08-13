096d63852bb09cd9f2e5f7817bb5170711496b69
Chris Bongers <chrisbongers@gmail.com>
2021-08-13 08:36:04 +0200

    Added 3D pan effect

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,72 @@ void main() async {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: MyApp(),
-      ),
+      home: MyApp(),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Offset _offset = Offset.zero;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Hello World 👋',
-        textDirection: TextDirection.ltr,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter 3D Card'),
+      ),
+      body: Center(
+        child: Transform(
+          transform: Matrix4.identity()
+            ..setEntry(3, 2, 0.001) // perspective
+            ..rotateX(0.01 * _offset.dy) // changed
+            ..rotateY(-0.01 * _offset.dx), // changed
+          alignment: FractionalOffset.center,
+          child: GestureDetector(
+            // new
+            onPanUpdate: (details) => setState(() => _offset += details.delta),
+            onDoubleTap: () => setState(() => _offset = Offset.zero),
+            child: FractionallySizedBox(
+              widthFactor: 0.7,
+              heightFactor: 0.9,
+              child: Image(image: AssetImage('DailyDevTips.png')),
+            ),
+          ),
+        ),
       ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: Text('Flutter 3D Card'), // changed
+  //     ),
+  //     body: Center(
+  //       child: Transform(
+  //         transform: Matrix4.identity()
+  //           ..setEntry(3, 2, 0.001) // perspective
+  //           ..rotateX(0.01 * _offset.dy) // changed
+  //           ..rotateY(-0.01 * _offset.dx), // changed
+  //         alignment: FractionalOffset.center,
+  //         child: GestureDetector(
+  //           // new
+  //           onPanUpdate: (details) => setState(() => _offset += details.delta),
+  //           onDoubleTap: () => setState(() => _offset = Offset.zero),
+  //           child: FractionallySizedBox(
+  //             widthFactor: 0.7,
+  //             heightFactor: 0.9,
+  //             child: Image(image: AssetImage('DailyDevTips.png')),
+  //           ),
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
 }

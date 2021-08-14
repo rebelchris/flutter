eadc4058442d9c4c4437008da7f694929bee0548
Chris Bongers <chrisbongers@gmail.com>
2021-08-14 20:18:28 +0200

    Added dialog screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ void main() async {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dialog buttons'),
+        ),
         body: MyApp(),
       ),
     ),
@@ -15,9 +18,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        'Hello World 👋',
-        textDirection: TextDirection.ltr,
+      child: ElevatedButton(
+        child: Text("Click me for a dialog"),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                backgroundColor: Colors.purple,
+                title: Text(
+                  "Do you agree?",
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                content: Text(
+                  "With this ridiculous statement.",
+                  style: TextStyle(color: Colors.white),
+                ),
+                actions: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.redAccent,
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("Disagree"),
+                  ),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                      ),
+                      onPressed: () {},
+                      child: Text("Agree")),
+                ],
+              );
+            },
+          );
+        },
       ),
     );
   }
